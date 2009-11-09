2fd494ae2a5c2a4ff6acccc91c549e247182e96d
thatcher <thatcher@christopher-thatchers-macbook-air.local>
2009-11-09 11:33:44 -0500

    added share this to global links

--- templates/site/html/header.js
+++ templates/site/html/header.js
@@ -25,6 +25,12 @@
            <li><a href={$.env('root')+'releases'}>releases</a></li> |
            <li><a href={$.env('root')+'docs'}>docs</a></li> |
            <li><a href={$.env('root')+'support'}>support</a></li> |
+           <li>
+        <script type="text/javascript" 
+                src="http://w.sharethis.com/widget/?tabs=web%2Cpost%2Cemail&amp;charset=utf-8&amp;style=default&amp;publisher=b0f530fc-b206-4fb8-b8a8-478191e675c2&amp;headerbg=%23c20000&amp;linkfg=%23c20000">
+                <!-- -->
+        </script>
+           </li>
 	   </ul>
 	</div>
 </block>
